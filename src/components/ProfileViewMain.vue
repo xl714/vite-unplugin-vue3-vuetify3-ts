928dcf9849a88ae3001381c530c14c3559488426
xl714 <xl714@users.noreply.github.com>
2022-11-07 12:26:14 +0100

    Dounut chartjs OK

--- src/components/ProfileViewMain.vue
+++ src/components/ProfileViewMain.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Profile } from '../classes/profile';
+import { BarChart } from '../components/BarChart';
 export interface Props {
   profile: Profile;
 }
@@ -18,6 +19,66 @@ const emitOpenEditFormProfile = () => {
   emits('onEmitOpenEditFormProfile', parseInt(props.profile.id));
   console.log('emitOpenEditFormProfile after emit');
 };
+
+/***************** CHART *****************/
+import { shuffle } from 'lodash';
+import { DoughnutChart, useDoughnutChart } from 'vue-chart-3';
+import { Chart, ChartData, ChartOptions, registerables } from 'chart.js';
+import MyComponent from './MyComponent.vue';
+
+Chart.register(...registerables);
+
+const dataValues = ref([30, 40, 60, 70, 5]);
+const dataLabels = ref(['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre']);
+const toggleLegend = ref(true);
+
+const testData = computed<ChartData<'doughnut'>>(() => ({
+  labels: dataLabels.value,
+  datasets: [
+    {
+      data: dataValues.value,
+      backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED'],
+    },
+  ],
+}));
+
+const options = computed<ChartOptions<'doughnut'>>(() => ({
+  scales: {
+    myScale: {
+      type: 'logarithmic',
+      position: toggleLegend.value ? 'left' : 'right',
+    },
+  },
+  plugins: {
+    legend: {
+      position: toggleLegend.value ? 'top' : 'bottom',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Doughnut Chart',
+    },
+  },
+}));
+
+const { doughnutChartProps, doughnutChartRef } = useDoughnutChart({
+  chartData: testData,
+  options,
+});
+
+let index = ref(20);
+
+const shuffleData = () => {
+  // dataValues.value = shuffle(dataValues.value);
+  dataValues.value.push(index.value);
+  dataLabels.value.push('Other' + index.value);
+  console.log(dataValues.value);
+  console.log(doughnutChartRef.value.chartInstance);
+  index.value++;
+};
+
+const switchLegend = () => {
+  toggleLegend.value = !toggleLegend.value;
+};
 </script>
 
 <template>
@@ -35,6 +96,11 @@ const emitOpenEditFormProfile = () => {
         >
           <i-mdi:edit />
         </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" class="">
+        <DoughnutChart v-bind="doughnutChartProps" />
       </v-col>
     </v-row>
   </v-container>
